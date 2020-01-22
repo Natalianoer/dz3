d83d99e1c4b06152ea50d72f02f6abf76cece630
Natalia <zjecutiez@gmail.com>
2020-01-22 17:10:40 +0300

    доработка камеры

--- camera.js
+++ camera.js
@@ -1,6 +1,8 @@
-function Camera(state, mode) {
+function Camera(state, mode, record, live) {
     Devices.call(this, state);
     this._mode = mode;
+    this._record = record;
+    this._live = live;
     this.dayMode = function() {
       this.style.backgrounColor = 'white';
     };
@@ -8,8 +10,16 @@ function Camera(state, mode) {
     this.nightMode = function() {
       this.style.backgrounColor = 'black';
     };
-    
+
     this.getMode = function () {
       return this._mode;
     };
+
+    this.getType = function () {
+      if ((type = this._record) || (type = this._live)) {
+        return this._state;
+      }
+    };
+
+
 }
